5c7bd2602e2db11df4a2eac69459d9c1a3a98425
Timothy Woodby <timwoodby@gmail.com>
2022-05-03 13:49:17 -0400

    udpated with library and working example

--- android/app/src/main/java/com/twoodby/mystringerapp/MainActivity.kt
+++ android/app/src/main/java/com/twoodby/mystringerapp/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.twoodby.mystringerapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.twoodby.mystringerapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private var myStringer: stringer.MyStringer = stringer.MyStringer("")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
+
+    override fun onResume() {
+        super.onResume()
+
+
+        // Setup Loading Button
+        binding.buttonLoadText.setOnClickListener {
+            myStringer = stringer.MyStringer(binding.inputText.text.toString())
+            binding.textLoaded.text = binding.inputText.text.toString()
+        }
+
+        binding.buttonFlipText.setOnClickListener {
+            binding.textOutput.text = myStringer.flipValue()
+        }
+
+        binding.buttonCrazyText.setOnClickListener {
+            binding.textOutput.text = myStringer.crazyCamel()
+        }
+
+
+    }
+
+
 }
