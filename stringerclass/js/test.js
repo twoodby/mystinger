5ccd0ad1460d83b88fc3349ae6a27e5cf0c326a6
Timothy Woodby <timwoodby@gmail.com>
2022-05-04 13:43:31 -0400

    updated

--- stringerclass/js/test.js
+++ stringerclass/js/test.js
@@ -1,7 +1,10 @@
 // MY GUIDE: https://medium.com/@kentquirk/how-to-use-gopherjs-to-turn-go-code-into-a-javascript-library-1e947703db7a
 
+const prompt = require('prompt-sync')();
+const inData = prompt('What is the input text?');
+
 m = require("./js.js");
-st = m.newStringer("abcdefg")
+st = m.newStringer(inData)
 
 st.flipValue().then((r) =>  {
     console.log("Flipped Text: " + r)
